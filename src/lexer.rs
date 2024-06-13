7d66e6facf53b471c12422acd40f591314c9d15c
Ruse Alexandru-Cristian <alexruse99@outlook.com>
2024-06-13 20:46:45 +0300

    Added tokens and lexer

---
+++ src/lexer.rs
@@ -0,0 +1,493 @@
+use crate::token::{lookup_ident, Token, TokenKind};
+
+pub struct Lexer {
+    input: Vec<char>,
+    position: usize,
+    read_position: usize,
+    ch: char,
+}
+
+impl Lexer {
+    pub fn new(input: &str) -> Self {
+        let mut lexer = Lexer {
+            input: input.chars().collect(),
+            position: 0,
+            read_position: 0,
+            ch: Default::default(),
+        };
+
+        lexer.read_char();
+
+        return lexer;
+    }
+
+    fn read_char(&mut self) {
+        if self.read_position >= self.input.len() {
+            self.ch = '\0';
+        } else {
+            self.ch = self.input[self.read_position];
+        }
+        self.position = self.read_position;
+        self.read_position += 1;
+    }
+
+    pub fn next_token(&mut self) -> Token {
+        self.skip_whitespace();
+        let token = match self.ch {
+            '=' => {
+                if self.peek_char() == '=' {
+                    self.read_char();
+                    Token {
+                        kind: TokenKind::Eq,
+                        literal: String::from("=="),
+                    }
+                } else {
+                    Lexer::new_token(TokenKind::Assign, self.ch)
+                }
+            }
+            ';' => Lexer::new_token(TokenKind::Semicolon, self.ch),
+            ',' => Lexer::new_token(TokenKind::Comma, self.ch),
+            '(' => Lexer::new_token(TokenKind::Lparen, self.ch),
+            ')' => Lexer::new_token(TokenKind::Rparen, self.ch),
+            '{' => Lexer::new_token(TokenKind::Lbrace, self.ch),
+            '}' => Lexer::new_token(TokenKind::Rbrace, self.ch),
+            '+' => Lexer::new_token(TokenKind::Plus, self.ch),
+            '-' => Lexer::new_token(TokenKind::Minus, self.ch),
+            '*' => Lexer::new_token(TokenKind::Asterisk, self.ch),
+            '/' => Lexer::new_token(TokenKind::Slash, self.ch),
+            '!' => {
+                if self.peek_char() == '=' {
+                    self.read_char();
+                    Token {
+                        kind: TokenKind::NotEq,
+                        literal: String::from("!="),
+                    }
+                } else {
+                    Lexer::new_token(TokenKind::Bang, self.ch)
+                }
+            }
+            '\0' => Token {
+                kind: TokenKind::Eof,
+                literal: "".to_string(),
+            },
+            '>' => Lexer::new_token(TokenKind::Gt, self.ch),
+            '<' => Lexer::new_token(TokenKind::Lt, self.ch),
+
+            _ => {
+                return if Lexer::is_letter(self.ch) {
+                    let literal = self.read_identifier();
+                    let kind = lookup_ident(&literal);
+                    Token { kind, literal }
+                } else if Lexer::is_digit(self.ch) {
+                    let kind = TokenKind::Int;
+                    let literal = self.read_number();
+                    Token { kind, literal }
+                } else {
+                    Lexer::new_token(TokenKind::Illegal, self.ch)
+                }
+            }
+        };
+
+        self.read_char();
+
+        return token;
+    }
+
+    fn is_digit(ch: char) -> bool {
+        ch.is_numeric()
+    }
+
+    fn peek_char(&self) -> char {
+        return if self.read_position >= self.input.len() {
+            '\0'
+        } else {
+            self.input[self.read_position]
+        };
+    }
+
+    fn read_number(&mut self) -> String {
+        let mut num = String::from("");
+
+        while Lexer::is_digit(self.ch) {
+            num.push(self.ch);
+            self.read_char();
+        }
+
+        num
+    }
+
+    fn skip_whitespace(&mut self) {
+        while self.ch.is_ascii_whitespace() {
+            self.read_char();
+        }
+    }
+
+    fn is_letter(ch: char) -> bool {
+        ch.is_alphabetic() || ch == '_'
+    }
+
+    fn read_identifier(&mut self) -> String {
+        let mut identifier = String::new();
+
+        while Lexer::is_letter(self.ch) {
+            identifier.push(self.ch);
+            self.read_char();
+        }
+
+        return identifier;
+    }
+
+    fn new_token(kind: TokenKind, ch: char) -> Token {
+        Token {
+            kind,
+            literal: ch.to_string(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::token::{Token, TokenKind};
+
+    use super::Lexer;
+
+    #[test]
+    fn test_next_token() {
+        // r#""# means its a raw literal string
+        let input = r#"
+        let five = 5;
+        let ten = 10;
+        
+        let add = fn(x,y) {
+            x + y;
+        };
+
+        let result = add(five, ten);
+        !-/*5;
+        5 < 10 > 5;
+
+        if (5 < 10) {
+            return true;
+        } else {
+            return false;
+        }
+
+        10 == 10;
+        10 != 9;
+        "#;
+
+        let expected: Vec<Token> = vec![
+            Token {
+                kind: TokenKind::Let,
+                literal: "let".to_string(),
+            },
+            Token {
+                kind: TokenKind::Ident,
+                literal: "five".to_string(),
+            },
+            Token {
+                kind: TokenKind::Assign,
+                literal: "=".to_string(),
+            },
+            Token {
+                kind: TokenKind::Int,
+                literal: "5".to_string(),
+            },
+            Token {
+                kind: TokenKind::Semicolon,
+                literal: ";".to_string(),
+            },
+            Token {
+                kind: TokenKind::Let,
+                literal: "let".to_string(),
+            },
+            Token {
+                kind: TokenKind::Ident,
+                literal: "ten".to_string(),
+            },
+            Token {
+                kind: TokenKind::Assign,
+                literal: "=".to_string(),
+            },
+            Token {
+                kind: TokenKind::Int,
+                literal: "10".to_string(),
+            },
+            Token {
+                kind: TokenKind::Semicolon,
+                literal: ";".to_string(),
+            },
+            Token {
+                kind: TokenKind::Let,
+                literal: "let".to_string(),
+            },
+            Token {
+                kind: TokenKind::Ident,
+                literal: "add".to_string(),
+            },
+            Token {
+                kind: TokenKind::Assign,
+                literal: "=".to_string(),
+            },
+            Token {
+                kind: TokenKind::Function,
+                literal: "fn".to_string(),
+            },
+            Token {
+                kind: TokenKind::Lparen,
+                literal: "(".to_string(),
+            },
+            Token {
+                kind: TokenKind::Ident,
+                literal: "x".to_string(),
+            },
+            Token {
+                kind: TokenKind::Comma,
+                literal: ",".to_string(),
+            },
+            Token {
+                kind: TokenKind::Ident,
+                literal: "y".to_string(),
+            },
+            Token {
+                kind: TokenKind::Rparen,
+                literal: ")".to_string(),
+            },
+            Token {
+                kind: TokenKind::Lbrace,
+                literal: "{".to_string(),
+            },
+            Token {
+                kind: TokenKind::Ident,
+                literal: "x".to_string(),
+            },
+            Token {
+                kind: TokenKind::Plus,
+                literal: "+".to_string(),
+            },
+            Token {
+                kind: TokenKind::Ident,
+                literal: "y".to_string(),
+            },
+            Token {
+                kind: TokenKind::Semicolon,
+                literal: ";".to_string(),
+            },
+            Token {
+                kind: TokenKind::Rbrace,
+                literal: "}".to_string(),
+            },
+            Token {
+                kind: TokenKind::Semicolon,
+                literal: ";".to_string(),
+            },
+            Token {
+                kind: TokenKind::Let,
+                literal: "let".to_string(),
+            },
+            Token {
+                kind: TokenKind::Ident,
+                literal: "result".to_string(),
+            },
+            Token {
+                kind: TokenKind::Assign,
+                literal: "=".to_string(),
+            },
+            Token {
+                kind: TokenKind::Ident,
+                literal: "add".to_string(),
+            },
+            Token {
+                kind: TokenKind::Lparen,
+                literal: "(".to_string(),
+            },
+            Token {
+                kind: TokenKind::Ident,
+                literal: "five".to_string(),
+            },
+            Token {
+                kind: TokenKind::Comma,
+                literal: ",".to_string(),
+            },
+            Token {
+                kind: TokenKind::Ident,
+                literal: "ten".to_string(),
+            },
+            Token {
+                kind: TokenKind::Rparen,
+                literal: ")".to_string(),
+            },
+            Token {
+                kind: TokenKind::Semicolon,
+                literal: ";".to_string(),
+            },
+            Token {
+                kind: TokenKind::Bang,
+                literal: "!".to_string(),
+            },
+            Token {
+                kind: TokenKind::Minus,
+                literal: "-".to_string(),
+            },
+            Token {
+                kind: TokenKind::Slash,
+                literal: "/".to_string(),
+            },
+            Token {
+                kind: TokenKind::Asterisk,
+                literal: "*".to_string(),
+            },
+            Token {
+                kind: TokenKind::Int,
+                literal: "5".to_string(),
+            },
+            Token {
+                kind: TokenKind::Semicolon,
+                literal: ";".to_string(),
+            },
+            Token {
+                kind: TokenKind::Int,
+                literal: "5".to_string(),
+            },
+            Token {
+                kind: TokenKind::Lt,
+                literal: "<".to_string(),
+            },
+            Token {
+                kind: TokenKind::Int,
+                literal: "10".to_string(),
+            },
+            Token {
+                kind: TokenKind::Gt,
+                literal: ">".to_string(),
+            },
+            Token {
+                kind: TokenKind::Int,
+                literal: "5".to_string(),
+            },
+            Token {
+                kind: TokenKind::Semicolon,
+                literal: ";".to_string(),
+            },
+            Token {
+                kind: TokenKind::If,
+                literal: "if".to_string(),
+            },
+            Token {
+                kind: TokenKind::Lparen,
+                literal: "(".to_string(),
+            },
+            Token {
+                kind: TokenKind::Int,
+                literal: "5".to_string(),
+            },
+            Token {
+                kind: TokenKind::Lt,
+                literal: "<".to_string(),
+            },
+            Token {
+                kind: TokenKind::Int,
+                literal: "10".to_string(),
+            },
+            Token {
+                kind: TokenKind::Rparen,
+                literal: ")".to_string(),
+            },
+            Token {
+                kind: TokenKind::Lbrace,
+                literal: "{".to_string(),
+            },
+            Token {
+                kind: TokenKind::Return,
+                literal: "return".to_string(),
+            },
+            Token {
+                kind: TokenKind::True,
+                literal: "true".to_string(),
+            },
+            Token {
+                kind: TokenKind::Semicolon,
+                literal: ";".to_string(),
+            },
+            Token {
+                kind: TokenKind::Rbrace,
+                literal: "}".to_string(),
+            },
+            Token {
+                kind: TokenKind::Else,
+                literal: "else".to_string(),
+            },
+            Token {
+                kind: TokenKind::Lbrace,
+                literal: "{".to_string(),
+            },
+            Token {
+                kind: TokenKind::Return,
+                literal: "return".to_string(),
+            },
+            Token {
+                kind: TokenKind::False,
+                literal: "false".to_string(),
+            },
+            Token {
+                kind: TokenKind::Semicolon,
+                literal: ";".to_string(),
+            },
+            Token {
+                kind: TokenKind::Rbrace,
+                literal: "}".to_string(),
+            },
+            Token {
+                kind: TokenKind::Int,
+                literal: "10".to_string(),
+            },
+            Token {
+                kind: TokenKind::Eq,
+                literal: "==".to_string(),
+            },
+            Token {
+                kind: TokenKind::Int,
+                literal: "10".to_string(),
+            },
+            Token {
+                kind: TokenKind::Semicolon,
+                literal: ";".to_string(),
+            },
+            Token {
+                kind: TokenKind::Int,
+                literal: "10".to_string(),
+            },
+            Token {
+                kind: TokenKind::NotEq,
+                literal: "!=".to_string(),
+            },
+            Token {
+                kind: TokenKind::Int,
+                literal: "9".to_string(),
+            },
+            Token {
+                kind: TokenKind::Semicolon,
+                literal: ";".to_string(),
+            },
+            Token {
+                kind: TokenKind::Eof,
+                literal: "".to_string(),
+            },
+        ];
+
+        let mut lexer = Lexer::new(input);
+        for (idx, exp_token) in expected.into_iter().enumerate() {
+            let recv_token = lexer.next_token();
+            assert_eq!(
+                exp_token.kind, recv_token.kind,
+                "tests[({idx}] - token type wrong, expected={}, got={}",
+                exp_token.kind, recv_token.kind
+            );
+            assert_eq!(
+                exp_token.literal, recv_token.literal,
+                "tests[{idx}] - token literal wrong, expected={}, got={}",
+                exp_token.literal, recv_token.literal
+            )
+        }
+    }
+}
---
+++ src/token.rs
@@ -0,0 +1,91 @@
+use std::fmt::Display;
+
+#[derive(PartialEq, Debug)]
+pub struct Token {
+    pub kind: TokenKind,
+    pub literal: String,
+}
+
+#[derive(PartialEq, Debug)]
+pub enum TokenKind {
+    Illegal,
+    Eof,
+
+    Ident,
+    Int,
+
+    Assign,
+    Plus,
+    Minus,
+    Asterisk,
+    Slash,
+    Bang,
+
+    Lt,
+    Gt,
+    Eq,
+    NotEq,
+
+    Comma,
+    Semicolon,
+
+    Lparen,
+    Rparen,
+    Lbrace,
+    Rbrace,
+
+    Function,
+    Let,
+    True,
+    False,
+    If,
+    Else,
+    Return,
+}
+
+impl Display for TokenKind {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            TokenKind::Illegal => write!(f, "Illegal"),
+            TokenKind::Eof => write!(f, "Eof"),
+            TokenKind::Ident => write!(f, "Ident"),
+            TokenKind::Int => write!(f, "Int"),
+            TokenKind::Assign => write!(f, "Assign"),
+            TokenKind::Plus => write!(f, "+"),
+            TokenKind::Minus => write!(f, "-"),
+            TokenKind::Asterisk => write!(f, "*"),
+            TokenKind::Slash => write!(f, "/"),
+            TokenKind::Bang => write!(f, "!"),
+            TokenKind::Gt => write!(f, ">"),
+            TokenKind::Lt => write!(f, "<"),
+            TokenKind::Eq => write!(f, "=="),
+            TokenKind::NotEq => write!(f, "!="),
+            TokenKind::Comma => write!(f, ","),
+            TokenKind::Semicolon => write!(f, ";"),
+            TokenKind::Lparen => write!(f, "("),
+            TokenKind::Rparen => write!(f, ")"),
+            TokenKind::Lbrace => write!(f, "{{"),
+            TokenKind::Rbrace => write!(f, "}}"),
+            TokenKind::Function => write!(f, "Function"),
+            TokenKind::Let => write!(f, "Let"),
+            TokenKind::True => write!(f, "True"),
+            TokenKind::False => write!(f, "False"),
+            TokenKind::If => write!(f, "If"),
+            TokenKind::Else => write!(f, "Else"),
+            TokenKind::Return => write!(f, "Return"),
+        }
+    }
+}
+
+pub fn lookup_ident(identifier: &String) -> TokenKind {
+    match identifier.as_str() {
+        "fn" => TokenKind::Function,
+        "let" => TokenKind::Let,
+        "true" => TokenKind::True,
+        "false" => TokenKind::False,
+        "if" => TokenKind::If,
+        "else" => TokenKind::Else,
+        "return" => TokenKind::Return,
+        _ => TokenKind::Ident,
+    }
+}
